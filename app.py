0e8dc2bb22a2dff38c5240fc7b23d775495116bc
Alkan <91989029+alkan-iztech@users.noreply.github.com>
2022-02-12 11:07:27 +0300

    tmep comment

--- app.py
+++ app.py
@@ -30,12 +30,13 @@ mail = Mail(app)
 
 BOOK_RECOM_ENV = environ.get("BOOK_RECOM_ENV")
 if BOOK_RECOM_ENV == 'prod':
+    # app.debug = False
     app.config = environ.get("JAWSDB_URL")
 else:
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/recomm_system'
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = "filesystem"
 Session(app)
